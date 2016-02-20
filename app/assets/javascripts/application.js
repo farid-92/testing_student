2cbd83de8849fb957425e1119751a1d943340dce
Farid <farid.babazov@gmail.com>
2016-02-20 13:32:15 +0600

    Добавил flash сообщения

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+//this function calls alert close method to close an alert
+function clearFlash() {
+    $(".alert").alert('close');
+}
+//this function calls clearFlash method after 3 seconds
+var clearFlashOnReady = function () {
+    setTimeout(clearFlash, 3000);
+};
