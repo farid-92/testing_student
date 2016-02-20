df753e6fda6b2aae0c658814b8bd7d0a1a070e2f
Farid <farid.babazov@gmail.com>
2016-02-20 18:11:03 +0600

    Добавил в миграцию поле test_id

--- db/migrate/20160220094709_create_results.rb
+++ db/migrate/20160220094709_create_results.rb
@@ -1,8 +1,11 @@
 class CreateResults < ActiveRecord::Migration
   def change
     create_table :results do |t|
-      t.string :student
-      t.string :correct
+      t.string :name
+      t.string :surname
+      t.string :group
+      t.string :total_point
+      t.references :test, index: true, foreign_key: true
 
       t.timestamps null: false
     end
